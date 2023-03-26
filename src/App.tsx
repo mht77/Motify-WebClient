a2ec696ee69e0735530f8019df82f05ff6591a99
Mohammad Toosi <m.h.t.1377@gmail.com>
2023-03-26 13:50:28 +0200

    add mui, fonts and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import './App.css';
+import {Button, createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h3>
-          Hello World!
-        </h3>
-      </header>
-    </div>
+    return (
+      <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <div className="App">
+          <header className="App-header">
+            <h3>
+              Hello World!
+            </h3>
+              <Button variant='outlined'>
+                    Click me!
+              </Button>
+          </header>
+        </div>
+      </ThemeProvider>
   );
 }
 
