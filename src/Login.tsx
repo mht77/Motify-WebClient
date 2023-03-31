24b549a3bdbdcc5e4c39c8af194b6e92bfdaab33
Mohammad Toosi <m.h.t.1377@gmail.com>
2023-03-31 16:06:00 +0200

    add google sso

--- src/Login.tsx
+++ src/Login.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Alert, Button, Snackbar, TextField} from "@mui/material";
 import {validateEmail, LoggedInProps} from "./Auth";
 import {axiosClient} from "./axiosClient";
+import {GoogleLogin, GoogleOAuthProvider} from "@react-oauth/google";
 
 const Login = (props: LoggedInProps) => {
     const [open, setOpen] = React.useState(false);
@@ -18,23 +19,43 @@ const Login = (props: LoggedInProps) => {
         setOpen(false);
     };
 
+    const setToken = (token: string) => {
+        localStorage.setItem('token', token);
+        props.setLoggedIn(true);
+    }
 
     const login = async () => {
         await axiosClient.post('token/', {'username': email, password}).then(res => {
             if (res.status === 200) {
-                localStorage.setItem('token', res.data.access);
-                props.setLoggedIn(true);
+               setToken(res.data.access);
             }
             else
                 setOpen(true);
         }).catch(err => {console.log(err); setOpen(true)});
     }
 
+    const googleLogin = async (data: any) => {
+        await axiosClient.post('google/', {'token': data.credential}).then(res => {
+            setToken(res.data.access);
+        }).catch(err => {console.log(err); setOpen(true)});
+    };
+
     return (
         <div>
             <TextField error={erEmail} onChange={(e) => onEmailChange(e.target.value)} variant='outlined' label='Email' type='email'/> <br/><br/>
             <TextField onChange={(e)=>setPassword(e.target.value)} variant='outlined' label='Password' type='password'/> <br/><br/>
-            <Button variant='contained' onClick={()=>login()}> Login </Button>
+            <Button variant='contained' onClick={()=>login()}> Login </Button><br/><br/>
+            <Button>
+                <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID!.toString()}>
+                    <GoogleLogin
+                        shape={"circle"}
+                        theme={"filled_black"}
+                        size={"large"}
+                        onSuccess={googleLogin}
+                        onError={()=>setOpen(true)}
+                    />
+                </GoogleOAuthProvider>
+            </Button>
             <Snackbar
                 anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
                 open={open}
