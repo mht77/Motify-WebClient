a619ea67a3eaefe78ee725ec5179b5a3ee7fb4ab
Mohammad Toosi <m.h.t.1377@gmail.com>
2023-04-08 23:25:01 +0200

    mock player

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,14 @@ import {
     Stack,
     ThemeProvider
 } from "@mui/material";
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
 import Grid from '@mui/material/Unstable_Grid2';
 import Menu from "./Menu";
 import Auth from "./Auth";
 import {axiosClient} from "./axiosClient";
+import IconButton from "@mui/material/IconButton";
 
 
 const darkTheme = createTheme({
@@ -69,13 +73,24 @@ function App() {
                   <Grid xs={0} md={2}></Grid>
                   <Grid xs={12} md={10}>
                       <Stack>
-                          <Item>body</Item>
+                          <Item>Welcome to Motify</Item>
                       </Stack>
                   </Grid>
               </Grid>
               <div className='left footer'>
-                  <Item>Footer</Item>
-                  <Item>Footer</Item>
+                  <Item sx={{height: '5rem'}}>
+                      <Box sx={{justifyContent: 'center',  display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+                          <IconButton aria-label="previous">
+                              {darkTheme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
+                          </IconButton>
+                          <IconButton aria-label="play/pause">
+                              <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+                          </IconButton>
+                          <IconButton aria-label="next">
+                              {darkTheme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
+                          </IconButton>
+                      </Box>
+                  </Item>
               </div>
           </Box>
       </ThemeProvider>
