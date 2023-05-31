a07bb40835ff1e476fc8ad219998959ca70d3580
Mohammad Toosi <m.h.t.1377@gmail.com>
2023-05-31 23:58:20 +0200

    change song from search view

--- src/SearchView.tsx
+++ src/SearchView.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Song} from "./types";
 import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import IconButton from "@mui/material/IconButton";
 
 interface searchProps {
     songs: Song[];
+    setSelectedSong: (song: Song) => void;
 }
 const SearchView = (props: searchProps) => {
 
@@ -15,10 +18,11 @@ const SearchView = (props: searchProps) => {
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <TableCell align='left'>Id</TableCell>
+                            <TableCell align="left"></TableCell>
                             <TableCell align='left'>Name</TableCell>
                             <TableCell align="left">Artist</TableCell>
                             <TableCell align="left">Album</TableCell>
+                            <TableCell align="left">Date Added</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
@@ -27,13 +31,15 @@ const SearchView = (props: searchProps) => {
                                 key={song.name}
                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                             >
-                                <TableCell component="th" scope="song">
-                                    {song.id}
+                                <TableCell align="left">
+                                    <IconButton onClick={()=> props.setSelectedSong(song)}>
+                                        <PlayArrowIcon sx={{height: 18, width: 18}}/>
+                                    </IconButton>
                                 </TableCell>
-                                <TableCell align="left">{song.name}</TableCell>
+                                <TableCell component="th" scope="song" align="left">{song.name}</TableCell>
                                 <TableCell align="left">{song.artist}</TableCell>
                                 <TableCell align="left">{song.album}</TableCell>
-                                {/*<TableCell align="right">{song.protein}</TableCell>*/}
+                                <TableCell align="left">{song.dateAdded}</TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
